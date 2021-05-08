cd2689ae96ca786f900d69eba92f4c924fc3f3c1
Tonyahn80 <tonyahn80@gmail.com>
2021-05-08 17:40:39 +0900

    2021-05-08 17:40

---
+++ day2/titanic.py
@@ -0,0 +1,23 @@
+class Titanic :
+
+    survival = '' # 생존여부(0사망, 1 생존)
+    pclass = '' # 승선권 클래스(1 - 1등석, 2 - 2등석, 3, 3등석)
+    sex = '' # 성별
+    age = '' # 승객 나이
+    sibsp = ''# 승객 이름
+    parch = '' # 동반한 형제, 자매, 배우자의 수
+    ticket = '' # 티켓의 고유 넘버
+    cabin = '' # 객실번호
+    embarked = '' # 승선한 항구명( C 캠브릿지, Q 퀸스타운, S 사우스햄프턴 )
+
+
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def main():
+        pass
+
+if __name__ == '__main__':
+    Titanic.main()
